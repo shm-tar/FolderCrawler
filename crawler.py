85a2907090c7d93610d0e5c3616df3725e648318
Taras Shmilyk <31892672+shm-tar@users.noreply.github.com>
2019-09-13 00:20:37 +0300

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,21 +1,21 @@
 import os, fnmatch
 
-dirs = [f for f in os.listdir(os.getcwd())]
+dirs = [f for f in os.listdir(os.getcwd())] # get all folders
 # print(dirs)
 
 pattern = "*.png"
 
 for entry in dirs:
 	try:
-		files_in_dir = os.listdir(entry)
+		files_in_dir = os.listdir(entry) # get all files inside a folder
 		
 		for file_entry in files_in_dir:
 			if fnmatch.fnmatch(file_entry, pattern):
 				print(file_entry + " found!")
 				input("Press any key to continue.")
 				print("Removing " + file_entry)
-				filename = os.path.join(os.getcwd(), entry, file_entry)
+				filename = os.path.join(os.getcwd(), entry, file_entry) # create a path to delete a file
 				os.remove(filename)
 				print("Removed.")
-	except(NotADirectoryError):
+	except(NotADirectoryError): # used to avoid getting inside a file, not a directory
 		pass
