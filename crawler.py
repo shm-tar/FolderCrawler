cb0a4757ed65430637852c61733d99a65728d0e5
Taras Shmilyk <31892672+shm-tar@users.noreply.github.com>
2020-03-17 15:13:54 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,20 +1,30 @@
 import os, fnmatch
+from send2trash import send2trash
 
-dirs = [f for f in os.listdir(os.getcwd())] # get all folders
+dirs = [f for f in os.listdir(os.getcwd())]
 
 pattern = "*.png"
+deletes = []
 
-for entry in dirs:
+for folder_entry in dirs:
 	try:
-		files_in_dir = os.listdir(entry) # get all files inside a folder
-		
+		files_in_dir = os.listdir(folder_entry)
 		for file_entry in files_in_dir:
 			if fnmatch.fnmatch(file_entry, pattern):
-				print(file_entry + " found!")
-				input("Press any key to continue.")
-				print("Removing " + file_entry)
-				filename = os.path.join(os.getcwd(), entry, file_entry) # create a path to delete a file
-				os.remove(filename)
-				print("Removed.")
-	except(NotADirectoryError): # used to avoid getting inside a file, not a directory
+				deletes.append(os.path.join(os.getcwd(), folder_entry, file_entry))
+	except(NotADirectoryError):
 		pass
+
+if len(deletes) == 0:
+	raise Exception(f"No files with {pattern} extension found.")
+
+for i in deletes:
+		print(i)
+
+input("\nPress any key to remove files.\n")
+try:
+	for i in deletes:
+		send2trash(i)
+	print("Removed files successfully.")
+except Exception as e:
+	raise Exception(e)
